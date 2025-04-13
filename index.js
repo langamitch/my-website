b53d4ba5c085ea05d3579ce58de5991964e7cd24
Wandile Langa <langamitch69@gmail.com>
2025-04-13 09:18:53 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+gsap.registerPlugin(ScrollTrigger);
+
+  gsap.to(".header", {
+    scrollTrigger: {
+      trigger: ".top",
+      start: "top top",
+      end: "bottom top",
+      scrub: true
+    },
+    y: -100,
+    opacity: 0,
+    ease: "power1.out"
+  });
+
+  window.addEventListener("DOMContentLoaded", () => {
+    gsap.to(".word span", {
+      y: 0,
+      opacity: 1,
+      duration: 0.6,
+      stagger: 0.05,
+      ease: "power3.out"
+    });
+  });
